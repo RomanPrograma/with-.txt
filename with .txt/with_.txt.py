8ef95652cb0cacdda349caf6cb2df89a1c013ac6
Roman <roma2000kuba@gmail.com>
2023-04-23 11:15:02 +0300

    ex_2 done

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,20 @@
-
+#try:
+#    _file = open('new file.txt', 'r')
+#    file_cont = _file.read()
+#    string = 'hello'
+#    if string in file_cont:
+#        print('yes')
+#except Exception as e:
+#    print(e)
+#finally:
+#    file_cont.close()
+try:
+    with open('new file.txt', 'r') as f:
+        file_cont = f.read().split()
+    striing = 'hello'
+    if striing in file_cont:
+        print('yes')
+    else:
+        print('no')
+except Exception as e:
+    print(f'error: {e}')
