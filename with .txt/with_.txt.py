b20c9690b349b552976c753d35b7ddf8bc14d682
Roman <roma2000kuba@gmail.com>
2023-04-23 11:58:20 +0300

    second variant (most correcrt mb)

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,13 @@
-
+try:
+    with open('New file.txt', 'r') as f:
+        line_number = int(input('Enter line number: ')) 
+        current_line_number = 1
+        for line in f:
+            if current_line_number == line_number:
+                print('Рядок знайдено!')
+                break
+            current_line_number += 1
+        else:
+            print(f"Рядок з номером {line_number} не знайдено")
+except Exception as e:
+    print(e)
