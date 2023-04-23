85ac3cc3b337d97571626f6fd5b88e99b8dbcecb
Roman <roma2000kuba@gmail.com>
2023-04-23 11:53:31 +0300

    ex_3 done

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,9 @@
+try:
+    with open('new file.txt', 'r') as f:
+        file_content = f.read()
+    x = file_content.split()
+    words = len(x)
+    print(f'words: {words}')
+except Exception as e:
+    print(f'error: {e}')
 
