5ad3ebae9954af45b0d13377d1f345815bd74b43
Roman <roma2000kuba@gmail.com>
2023-04-25 22:47:04 +0300

    ex_5 done

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,11 @@
 
+
+
+try:
+    with open("with.txt", "r") as f:
+        content = f.read()
+    for digit in content.split():
+        if digit.isdigit():
+            print(digit)
+except Exception as e:
+    print("Error", e)
