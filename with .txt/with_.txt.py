0373a3ac85b688ba6d510491ac95dd0ed4a5ba24
Roman <roma2000kuba@gmail.com>
2023-04-25 22:35:58 +0300

    ex_4 done

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,9 @@
-
+try:
+    with open("with.txt", "r") as f:
+        string = f.read()
+    for word in string.splitlines():
+        if 'am' in word:
+            print(word)
+except Exception as e:
+    print(e)
+    print("Error: " + str(e))
