bd99594c407de741c75728a9a9bf86364dafb983
Roman <roma2000kuba@gmail.com>
2023-04-26 22:27:43 +0300

    ex_7 done

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,15 @@
 
+try:
+    with open('with_.txt', 'r') as f:
+        cont = f.read()
+        n = input('Enter string that you want to change')
+        m = input('Enter string that you want to change with')
+        content = cont.replace(n, m)
+    with open('with_.txt', 'w') as f:
+       f.write(content)
+except Exception as e:
+    print(f'error: {e}')
+finally:
+    f.close()
+    print('Done, file closed')
+
