baa3674e4aafd4fec511a4a150317483536771d3
Roman <roma2000kuba@gmail.com>
2023-04-26 20:54:49 +0300

    ex_6 done

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,17 @@
 
+try:
+    with open( "with_.txt", "r" ) as f:
+        content = f.readlines()
+        n = int(input("Enter quantity lines: "))
+
+        for num, line in enumerate(content[-n:]):
+            print( line, end='')
+        print()
+        if n > len(content):
+            print( "Error: The file does not have that many lines", end= '' )
+        elif n < len(content):
+            print()
+except Exception as e:
+    print( "Error: " + str(e) )
+finally:
+    f.close()
