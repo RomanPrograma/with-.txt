4fcf61bf7a018cec1c2de04133d26ed1726cff65
Roman <roma2000kuba@gmail.com>
2023-04-27 23:30:00 +0300

    ex_ 9 done

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,9 @@
-
+try:
+    f = open( "with_.txt", "r" )
+    content = f.readlines()
+    content.sort()
+    open( "with_.txt", "w" ).writelines( content )
+except Exception as e:
+    print( "Error: " + str(e) )
+finally:
+    f.close()
