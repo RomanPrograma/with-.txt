9b63c4a91be527798bd5a659621b36bac79baf6e
Roman <roma2000kuba@gmail.com>
2023-04-27 18:03:28 +0300

    ex_8 done

--- with .txt/with_.txt.py
+++ with .txt/with_.txt.py
@@ -1,1 +1,17 @@
 
+try:
+    with open('with.txt', 'r') as f:
+         content = f.read()
+         lines = set()
+         for line in content.split():
+            if line in lines:
+                print('File contains duplicate lines')
+                break
+            lines.add(line)
+         else:
+            print('File does not contain duplicate lines')
+except Exception as e:
+    print(e)
+finally:
+    print('Execution completed')
+    f.close()
